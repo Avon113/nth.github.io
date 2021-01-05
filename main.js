6a3630e56e87fd4a0228b4154aed0a361be5fb38
Avon113 <garu097@gmail.com>
2021-01-05 19:48:57 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var x, y;
+
+var btnNoElement = document.getElementById('btnNo'),
+    btnYesElement = document.getElementById('btnYes');
+btnNoElement.onmouseover = function () {
+    x = Math.floor(((Math.random() - 0.5) * 2) * (window.innerWidth / 2));
+    y = Math.floor(((Math.random() - 0.5) * 2) * (window.innerHeight / 8));
+    btnNoElement.style.right = x + 'px';
+    btnNoElement.style.top = y + 'px';
+}
+
+btnYesElement.onclick = function () {
+    alert('Chúc mừng mày nhận ra sự thật 😉');
+}
+
