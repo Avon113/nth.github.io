39d8c21e14220dd0ee49fda5312f1df788285e09
Avon113 <garu097@gmail.com>
2021-01-05 20:20:18 +0700

    change all

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var btnNoElement = document.getElementById('btnNo'),
     btnYesElement = document.getElementById('btnYes');
 btnNoElement.onmouseover = function () {
     x = Math.floor(((Math.random() - 0.5) * 2) * (window.innerWidth / 2));
-    y = Math.floor(((Math.random() - 0.5) * 2) * (window.innerHeight / 8));
+    y = Math.floor(((Math.random() - 0.5) * 2) * (window.innerHeight / 5));
     btnNoElement.style.right = x + 'px';
     btnNoElement.style.top = y + 'px';
 }
